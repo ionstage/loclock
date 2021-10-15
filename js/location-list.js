f14b8196181b13e2a6505b3215b2cfbdb9179699
nasitra <nasitra@users.noreply.github.com>
2021-10-15 18:36:36 +0900

    Make method to find locations

--- js/location-list.js
+++ js/location-list.js
@@ -2,11 +2,20 @@
   'use strict';
 
   var moment = require('moment-timezone');
+  var helper = app.helper || require('./helper.js');
   var Location = app.Location || require('./location.js');
 
   var LocationList = function() {
     this.data = {};
     this.locations = this._createLocations(Location.DEFAULT_KEYS);
+  };
+
+  LocationList.prototype.findLocations = function(keys) {
+    return keys.map(function(key) {
+      return helper.find(this.locations, function(location) {
+        return (location.key === key);
+      });
+    }.bind(this));
   };
 
   LocationList.prototype.getItems = function(keys) {
