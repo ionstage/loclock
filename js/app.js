b985374070143c6b56408405745fd2e20ebe486f
nasitra <nasitra@users.noreply.github.com>
2021-10-20 19:06:30 +0900

    Refactor code for methods of 'clock' component

--- js/app.js
+++ js/app.js
@@ -12,255 +12,6 @@
   var NS_SVG = 'http://www.w3.org/2000/svg';
   var DEFAULT_LOCATIONS = ['America/New_York', 'Europe/London', 'Asia/Tokyo'];
 
-  function createCircle(o) {
-    var element = document.createElementNS(NS_SVG, 'circle');
-    for (var key in o) {
-      element.setAttribute(key, o[key]);
-    }
-    return element;
-  }
-
-  function createText(text, o) {
-    var element = document.createElementNS(NS_SVG, 'text');
-    element.appendChild(document.createTextNode(text));
-    for (var key in o) {
-      element.setAttribute(key, o[key]);
-    }
-    return element;
-  }
-
-  function createBoard(x, y, r) {
-    var board = document.createElementNS(NS_SVG, 'g');
-
-    board.appendChild(createCircle({
-      cx: x,
-      cy: y,
-      r: r,
-      'stroke-width': (r / 30).toFixed(1),
-      'class': 'circle',
-    }));
-
-    board.appendChild(createCircle({
-      cx: x,
-      cy: y,
-      r: (r / 45).toFixed(1),
-      fill: 'black',
-      'class': 'center-point',
-    }));
-
-    var center_spin = document.createElementNS(NS_SVG, 'g');
-    center_spin.setAttribute('class', 'center-spin');
-
-    center_spin.appendChild(createText('+00:00', {
-      x: x - 11,
-      y: y - 16,
-      'font-size': r / 6,
-      'class': 'text center-time',
-    }));
-
-    center_spin.appendChild(createText('RESET', {
-      x: x,
-      y: y + 32,
-      'font-size': r / 10,
-      'class': 'text center-reset',
-    }));
-
-    board.appendChild(center_spin);
-
-    var dif = Math.PI / 12;
-    var deg = 0;
-
-    for (var i = 0; i < 24; i++) {
-      var text = (i % 3 === 0) ? String((i - 6 >= 0) ? i - 6 : i + 18) : '・';
-      var fontSize = (text === '・') ? r / 12 : r / 4.5;
-      var rate = (text === '18' || text === '15' || text === '21') ? 0.04 : 0;
-      var dif_x = r * (0.8 - Math.abs(rate * Math.cos(deg))) * Math.cos(deg);
-      var dif_y = r * (0.8 - Math.abs(rate * Math.sin(deg))) * Math.sin(deg);
-
-      board.appendChild(createText(text, {
-        x: (x + dif_x).toFixed(1),
-        y: (y + dif_y).toFixed(1),
-        'font-size': fontSize.toFixed(),
-        'class': 'text',
-      }));
-
-      deg += dif;
-    }
-
-    return board;
-  }
-
-  function adjustBoard(board) {
-    forEachTextElement(board, function(element) {
-      var textBBox = element.getBBox();
-      var dy = +element.getAttribute('y') - (textBBox.y + textBBox.height / 2);
-      element.setAttribute('dy', dy);
-    });
-  }
-
-  function createPoint(x, y, r, locations, now, timeOffset) {
-    var containerElement = document.createElementNS(NS_SVG, 'g');
-    var pointItemMap = {};
-
-    locations.forEach(function(location) {
-      var locationName = location.name;
-      var date = new Date(location.getLocalTime(now));
-      var key = date.getTime() % (24 * 60 * 60 * 1000);
-      var pointItem = pointItemMap[key];
-
-      if (pointItem) {
-        pointItem.text +=  ', ' + locationName;
-        return;
-      }
-
-      pointItemMap[key] = {
-        text: locationName,
-        deg: (date.getHours() + (date.getMinutes() + timeOffset) / 60) / 24 * (Math.PI * 2) + Math.PI / 2,
-      };
-    });
-
-    for (var key in pointItemMap) {
-      var pointItem = pointItemMap[key];
-      var text = pointItem.text;
-      var deg = pointItem.deg;
-
-      containerElement.appendChild(createCircle({
-        cx: (x + r * Math.cos(deg)).toFixed(1),
-        cy: (y + r * Math.sin(deg)).toFixed(1),
-        r: (r / 20).toFixed(1),
-        'stroke-width': (r / 90).toFixed(1),
-        'class': 'circle',
-      }));
-
-      containerElement.appendChild(createText(text, {
-        x: x + r * Math.cos(deg),
-        y: y + r * Math.sin(deg),
-        'font-size': (r / 8).toFixed(),
-        'class': 'text',
-        'data-deg': deg,
-      }));
-    }
-
-    return containerElement;
-  }
-
-  function forEachTextElement(parent, method) {
-    var children = Array.prototype.slice.call(parent.childNodes);
-    children.forEach(function(element) {
-      if (element.nodeName === 'text') {
-        method(element);
-      }
-    });
-  }
-
-  function isBBoxOverlaid(bb0, bb1) {
-    return ((bb0.x <= bb1.x && bb0.x + bb0.width >= bb1.x) ||
-              (bb0.x >= bb1.x && bb1.x + bb1.width >= bb0.x)) &&
-            ((bb0.y <= bb1.y && bb0.y + bb0.height >= bb1.y) ||
-              (bb0.y >= bb1.y && bb1.y + bb1.height >= bb0.y));
-  }
-
-  function shrinkElement(element, width, height) {
-    var bb = element.getBBox();
-    var gbb = clock_view.globalBBox(bb);
-    var pattern = 0;
-
-    if ((gbb.x < 0 && (pattern = 1)) ||
-        (gbb.x + gbb.width > width && (pattern = 2)) ||
-        (gbb.y < 0 && (pattern = 3)) ||
-        (gbb.y + gbb.height > height && (pattern = 4))) {
-      element.setAttribute('font-size', +element.getAttribute('font-size') / 1.5);
-
-      var newbb = element.getBBox();
-      var value;
-
-      switch (pattern) {
-        case 1:
-          value = bb.x + bb.width - (newbb.x + newbb.width);
-          element.setAttribute('x', +element.getAttribute('x') + value);
-          break;
-        case 2:
-          value = newbb.x - bb.x;
-          element.setAttribute('x', +element.getAttribute('x') - value);
-          break;
-        case 3:
-          value = bb.y + bb.height - (newbb.y + newbb.height);
-          element.setAttribute('y', +element.getAttribute('y') + value);
-          break;
-        case 4:
-          value = newbb.y - bb.y;
-          element.setAttribute('y', +element.getAttribute('y') - value);
-          break;
-        default:
-          break;
-      }
-    }
-  }
-
-  function adjustPointText(point, x, y, r, width, height) {
-    var upper_elements = [];
-    var down_elements = [];
-    var elements = [];
-
-    forEachTextElement(point, function(element) {
-      var textBBox = element.getBBox();
-      var deg = +element.getAttribute('data-deg');
-      var dy = +element.getAttribute('y') - (textBBox.y + textBBox.height / 2);
-      var sin = Math.sin(deg);
-      var cos = Math.cos(deg);
-
-      element.setAttribute('x', x + (r * 1.125 + textBBox.width / 2 + (textBBox.height / 2) * sin * sin) * cos);
-      element.setAttribute('y', y + (r * 1.125 + textBBox.height / 2 + (textBBox.width / 8) * cos * cos) * sin);
-      element.setAttribute('dy', dy);
-      textBBox = element.getBBox();
-
-      if (textBBox.y + textBBox.height / 2 < y) {
-        upper_elements.push([element, textBBox]);
-      } else {
-        down_elements.push([element, textBBox]);
-      }
-    });
-
-    var ulen = upper_elements.length;
-    var dlen = down_elements.length;
-
-    upper_elements.sort(function(a, b) {
-      return (a[1].y < b[1].y) ? -1 : 1;
-    }).forEach(function(item, i) {
-      var el = item[0];
-      for (var j = i + 1; j < ulen; j++) {
-        var bb0 = item[1];
-        var bb1 = upper_elements[j][1];
-        if (!isBBoxOverlaid(bb0, bb1)) {
-          continue;
-        }
-        var dy = +el.getAttribute('dy') - ((bb0.y + bb0.height) - bb1.y);
-        el.setAttribute('dy', dy);
-      }
-      elements.push(el);
-    });
-
-    down_elements.sort(function(a, b) {
-      return (a[1].y > b[1].y) ? -1 : 1;
-    }).forEach(function(item, i) {
-      var el = item[0];
-      for (var j = i + 1; j < dlen; j++) {
-        var bb0 = item[1];
-        var bb1 = down_elements[j][1];
-        if (!isBBoxOverlaid(bb0, bb1)) {
-          continue;
-        }
-        var dy = +el.getAttribute('dy') + ((bb1.y + bb1.height) - bb0.y);
-        el.setAttribute('dy', dy);
-      }
-      elements.push(el);
-    });
-
-    elements.forEach(function(element) {
-      shrinkElement(element, width, height);
-    });
-  }
-
   var selectedLocations = [];
 
   var listToggle = (function() {
@@ -559,16 +310,16 @@
       }.bind(this));
     },
     updateBoard: function() {
-      var new_board = createBoard(this.x, this.y, this.r);
+      var new_board = this.createBoard(this.x, this.y, this.r);
       this.element.replaceChild(new_board, this.board_element);
-      adjustBoard(new_board);
+      this.adjustBoard(new_board);
       this.board_element = new_board;
       this.center_time_element = this.board_element.querySelector('.center-time');
     },
     updatePoint: function(now) {
-      var new_point = createPoint(this.x, this.y, this.r, selectedLocations, now, dial_spinner.timeOffset);
+      var new_point = this.createPoint(this.x, this.y, this.r, selectedLocations, now, dial_spinner.timeOffset);
       this.element.replaceChild(new_point, this.point_element);
-      adjustPointText(new_point, this.x, this.y, this.r, window.innerWidth, window.innerHeight);
+      this.adjustPointText(new_point, this.x, this.y, this.r, window.innerWidth, window.innerHeight);
       this.point_element = new_point;
     },
     updateCenter: function() {
@@ -594,6 +345,246 @@
         width: pt1.x - pt0.x,
         height: pt1.y - pt0.y,
       };
+    },
+    createCircle: function(o) {
+      var element = document.createElementNS(NS_SVG, 'circle');
+      for (var key in o) {
+        element.setAttribute(key, o[key]);
+      }
+      return element;
+    },
+    createText: function(text, o) {
+      var element = document.createElementNS(NS_SVG, 'text');
+      element.appendChild(document.createTextNode(text));
+      for (var key in o) {
+        element.setAttribute(key, o[key]);
+      }
+      return element;
+    },
+    createBoard: function(x, y, r) {
+      var board = document.createElementNS(NS_SVG, 'g');
+
+      board.appendChild(this.createCircle({
+        cx: x,
+        cy: y,
+        r: r,
+        'stroke-width': (r / 30).toFixed(1),
+        'class': 'circle',
+      }));
+
+      board.appendChild(this.createCircle({
+        cx: x,
+        cy: y,
+        r: (r / 45).toFixed(1),
+        fill: 'black',
+        'class': 'center-point',
+      }));
+
+      var center_spin = document.createElementNS(NS_SVG, 'g');
+      center_spin.setAttribute('class', 'center-spin');
+
+      center_spin.appendChild(this.createText('+00:00', {
+        x: x - 11,
+        y: y - 16,
+        'font-size': r / 6,
+        'class': 'text center-time',
+      }));
+
+      center_spin.appendChild(this.createText('RESET', {
+        x: x,
+        y: y + 32,
+        'font-size': r / 10,
+        'class': 'text center-reset',
+      }));
+
+      board.appendChild(center_spin);
+
+      var dif = Math.PI / 12;
+      var deg = 0;
+
+      for (var i = 0; i < 24; i++) {
+        var text = (i % 3 === 0) ? String((i - 6 >= 0) ? i - 6 : i + 18) : '・';
+        var fontSize = (text === '・') ? r / 12 : r / 4.5;
+        var rate = (text === '18' || text === '15' || text === '21') ? 0.04 : 0;
+        var dif_x = r * (0.8 - Math.abs(rate * Math.cos(deg))) * Math.cos(deg);
+        var dif_y = r * (0.8 - Math.abs(rate * Math.sin(deg))) * Math.sin(deg);
+
+        board.appendChild(this.createText(text, {
+          x: (x + dif_x).toFixed(1),
+          y: (y + dif_y).toFixed(1),
+          'font-size': fontSize.toFixed(),
+          'class': 'text',
+        }));
+
+        deg += dif;
+      }
+
+      return board;
+    },
+    adjustBoard: function(board) {
+      this.forEachTextElement(board, function(element) {
+        var textBBox = element.getBBox();
+        var dy = +element.getAttribute('y') - (textBBox.y + textBBox.height / 2);
+        element.setAttribute('dy', dy);
+      });
+    },
+    createPoint: function(x, y, r, locations, now, timeOffset) {
+      var containerElement = document.createElementNS(NS_SVG, 'g');
+      var pointItemMap = {};
+
+      locations.forEach(function(location) {
+        var locationName = location.name;
+        var date = new Date(location.getLocalTime(now));
+        var key = date.getTime() % (24 * 60 * 60 * 1000);
+        var pointItem = pointItemMap[key];
+
+        if (pointItem) {
+          pointItem.text +=  ', ' + locationName;
+          return;
+        }
+
+        pointItemMap[key] = {
+          text: locationName,
+          deg: (date.getHours() + (date.getMinutes() + timeOffset) / 60) / 24 * (Math.PI * 2) + Math.PI / 2,
+        };
+      });
+
+      for (var key in pointItemMap) {
+        var pointItem = pointItemMap[key];
+        var text = pointItem.text;
+        var deg = pointItem.deg;
+
+        containerElement.appendChild(this.createCircle({
+          cx: (x + r * Math.cos(deg)).toFixed(1),
+          cy: (y + r * Math.sin(deg)).toFixed(1),
+          r: (r / 20).toFixed(1),
+          'stroke-width': (r / 90).toFixed(1),
+          'class': 'circle',
+        }));
+
+        containerElement.appendChild(this.createText(text, {
+          x: x + r * Math.cos(deg),
+          y: y + r * Math.sin(deg),
+          'font-size': (r / 8).toFixed(),
+          'class': 'text',
+          'data-deg': deg,
+        }));
+      }
+
+      return containerElement;
+    },
+    forEachTextElement: function(parent, method) {
+      var children = Array.prototype.slice.call(parent.childNodes);
+      children.forEach(function(element) {
+        if (element.nodeName === 'text') {
+          method(element);
+        }
+      });
+    },
+    isBBoxOverlaid: function(bb0, bb1) {
+      return ((bb0.x <= bb1.x && bb0.x + bb0.width >= bb1.x) ||
+                (bb0.x >= bb1.x && bb1.x + bb1.width >= bb0.x)) &&
+              ((bb0.y <= bb1.y && bb0.y + bb0.height >= bb1.y) ||
+                (bb0.y >= bb1.y && bb1.y + bb1.height >= bb0.y));
+    },
+    shrinkElement: function(element, width, height) {
+      var bb = element.getBBox();
+      var gbb = clock_view.globalBBox(bb);
+      var pattern = 0;
+
+      if ((gbb.x < 0 && (pattern = 1)) ||
+          (gbb.x + gbb.width > width && (pattern = 2)) ||
+          (gbb.y < 0 && (pattern = 3)) ||
+          (gbb.y + gbb.height > height && (pattern = 4))) {
+        element.setAttribute('font-size', +element.getAttribute('font-size') / 1.5);
+
+        var newbb = element.getBBox();
+        var value;
+
+        switch (pattern) {
+          case 1:
+            value = bb.x + bb.width - (newbb.x + newbb.width);
+            element.setAttribute('x', +element.getAttribute('x') + value);
+            break;
+          case 2:
+            value = newbb.x - bb.x;
+            element.setAttribute('x', +element.getAttribute('x') - value);
+            break;
+          case 3:
+            value = bb.y + bb.height - (newbb.y + newbb.height);
+            element.setAttribute('y', +element.getAttribute('y') + value);
+            break;
+          case 4:
+            value = newbb.y - bb.y;
+            element.setAttribute('y', +element.getAttribute('y') - value);
+            break;
+          default:
+            break;
+        }
+      }
+    },
+    adjustPointText: function(point, x, y, r, width, height) {
+      var upper_elements = [];
+      var down_elements = [];
+      var elements = [];
+
+      this.forEachTextElement(point, function(element) {
+        var textBBox = element.getBBox();
+        var deg = +element.getAttribute('data-deg');
+        var dy = +element.getAttribute('y') - (textBBox.y + textBBox.height / 2);
+        var sin = Math.sin(deg);
+        var cos = Math.cos(deg);
+
+        element.setAttribute('x', x + (r * 1.125 + textBBox.width / 2 + (textBBox.height / 2) * sin * sin) * cos);
+        element.setAttribute('y', y + (r * 1.125 + textBBox.height / 2 + (textBBox.width / 8) * cos * cos) * sin);
+        element.setAttribute('dy', dy);
+        textBBox = element.getBBox();
+
+        if (textBBox.y + textBBox.height / 2 < y) {
+          upper_elements.push([element, textBBox]);
+        } else {
+          down_elements.push([element, textBBox]);
+        }
+      });
+
+      var ulen = upper_elements.length;
+      var dlen = down_elements.length;
+
+      upper_elements.sort(function(a, b) {
+        return (a[1].y < b[1].y) ? -1 : 1;
+      }).forEach(function(item, i) {
+        var el = item[0];
+        for (var j = i + 1; j < ulen; j++) {
+          var bb0 = item[1];
+          var bb1 = upper_elements[j][1];
+          if (!this.isBBoxOverlaid(bb0, bb1)) {
+            continue;
+          }
+          var dy = +el.getAttribute('dy') - ((bb0.y + bb0.height) - bb1.y);
+          el.setAttribute('dy', dy);
+        }
+        elements.push(el);
+      }.bind(this));
+
+      down_elements.sort(function(a, b) {
+        return (a[1].y > b[1].y) ? -1 : 1;
+      }).forEach(function(item, i) {
+        var el = item[0];
+        for (var j = i + 1; j < dlen; j++) {
+          var bb0 = item[1];
+          var bb1 = down_elements[j][1];
+          if (!this.isBBoxOverlaid(bb0, bb1)) {
+            continue;
+          }
+          var dy = +el.getAttribute('dy') + ((bb1.y + bb1.height) - bb0.y);
+          el.setAttribute('dy', dy);
+        }
+        elements.push(el);
+      }.bind(this));
+
+      elements.forEach(function(element) {
+        this.shrinkElement(element, width, height);
+      }.bind(this));
     },
   };
 
