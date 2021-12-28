0d58dfbc80c9112be948b9e5728e6f9a91fdf899
nasitra <nasitra@users.noreply.github.com>
2021-12-28 18:46:35 +0900

    Modify code for clicking button

--- js/components/button.js
+++ js/components/button.js
@@ -1,25 +1,38 @@
 (function(app) {
   'use strict';
 
+  var Draggable = app.Draggable || require('./draggable.js');
   var Events = app.Events || require('../base/events.js');
 
   var Button = function(el) {
     this.el = el;
     this._events = new Events();
+    this._draggable = new Draggable(this.el, {
+      onstart: this._onstart.bind(this),
+      onmove: function() { /* do nothing */ },
+      onend: this._onend.bind(this),
+    });
+    this._dragTarget = null;
   };
 
   Button.prototype.init = function() {
-    this.el.addEventListener('click', this._onclick.bind(this));
+    this._draggable.enable();
   };
 
   Button.prototype.on = function() {
     return Events.prototype.on.apply(this._events, arguments);
   };
 
-  Button.prototype._onclick = function(event) {
+  Button.prototype._onstart = function(event) {
     event.preventDefault();
-    event.stopPropagation();
-    this._events.emit('click');
+    this._dragTarget = event.target;
+  };
+
+  Button.prototype._onend = function(event) {
+    if (this._dragTarget === event.target) {
+      this._events.emit('click');
+    }
+    this._dragTarget = null;
   };
 
   if (typeof module !== 'undefined' && module.exports) {
