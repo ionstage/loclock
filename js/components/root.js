0ef43107b855a7bb7402aa1ac7a322d12a4dc0b8
nasitra <nasitra@users.noreply.github.com>
2022-08-23 21:08:56 +0900

    Remove dialog element

--- js/components/root.js
+++ js/components/root.js
@@ -5,7 +5,6 @@
   var helper = app.helper || require('../helper.js');
   var dom = app.dom || require('../dom.js');
   var Collection = app.Collection || require('../base/collection.js');
-  var Dialog = app.Dialog || require('./dialog.js');
   var Main = app.Main || require('./main.js');
   var Location = app.Location || require('../models/location.js');
 
@@ -18,7 +17,6 @@
       locations: this._locations,
       selectedLocations: this._selectedLocations,
     });
-    this._dialog = new Dialog(document.querySelector('.dialog'));
   };
 
   Root.prototype.init = function() {
