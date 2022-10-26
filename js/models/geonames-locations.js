9a02c56f2402642dc90e0c77add8dcde20f948b4
nasitra <nasitra@users.noreply.github.com>
2022-10-26 18:32:25 +0900

    Add 'enabled' property for 'GeoNamesLocations'

--- js/models/geonames-locations.js
+++ js/models/geonames-locations.js
@@ -4,11 +4,20 @@
   var Events = app.Events || require('../base/events.js');
 
   var GeoNamesLocations = function() {
+    this._enabled = false;
     this._events = new Events();
   };
 
   GeoNamesLocations.prototype.on = function() {
     return Events.prototype.on.apply(this._events, arguments);
+  };
+
+  GeoNamesLocations.prototype.setEnabled = function(enabled) {
+    if (enabled === this._enabled) {
+      return;
+    }
+    this._enabled = enabled;
+    this._events.emit(enabled ? 'enabled' : 'disabled');
   };
 
   if (typeof module !== 'undefined' && module.exports) {
