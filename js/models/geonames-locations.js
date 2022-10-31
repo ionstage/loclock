b3e944b0942b698a7b5faf5888c4d3ba7c9c0444
nasitra <nasitra@users.noreply.github.com>
2022-10-31 21:50:41 +0900

    Create items of GeoNames locations

--- js/models/geonames-locations.js
+++ js/models/geonames-locations.js
@@ -1,11 +1,13 @@
 (function(app) {
   'use strict';
 
+  var Collection = app.Collection || require('../base/collection.js');
   var Events = app.Events || require('../base/events.js');
 
   var GeoNamesLocations = function() {
     this._enabled = false;
     this._events = new Events();
+    this._items = this._createItems(this._events);
   };
 
   GeoNamesLocations.prototype.on = function() {
@@ -18,6 +20,14 @@
     }
     this._enabled = enabled;
     this._events.emit(enabled ? 'enabled' : 'disabled');
+  };
+
+  GeoNamesLocations.prototype._createItems = function(events) {
+    var items = new Collection();
+    items.on('add', events.emit.bind(events, 'add'));
+    items.on('remove', events.emit.bind(events, 'remove'));
+    items.on('reset', events.emit.bind(events, 'reset'));
+    return items;
   };
 
   if (typeof module !== 'undefined' && module.exports) {
