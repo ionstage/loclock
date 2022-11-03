5c33c276fb01a3b90e018cbe784b55cb8717d788
nasitra <nasitra@users.noreply.github.com>
2022-11-03 09:02:01 +0900

    Refactor code for theme inputs

--- js/components/preferences.js
+++ js/components/preferences.js
@@ -4,13 +4,15 @@
   var Button = app.Button || require('./button.js');
   var Events = app.Events || require('../base/events.js');
   var GeoNamesInputs = app.GeoNamesInputs || require('./geonames-inputs.js');
+  var ThemeInputs = app.ThemeInputs || require('./theme-inputs.js');
 
   var Preferences = function(el, props) {
     this.el = el;
-    this._themeAttrs = props.themeAttrs;
     this._events = new Events();
     this._hideButton = new Button(this.el.querySelector('.preferences-hide-button'));
-    this._themeFieldsetElement = this.el.querySelector('.preferences-fieldset-theme');
+    this._themeInputs = new ThemeInputs(this.el.querySelector('.preferences-inputs-theme'), {
+      themeAttrs: props.themeAttrs,
+    });
     this._geonamesInputs = new GeoNamesInputs(this.el.querySelector('.preferences-inputs-geonames'), {
       geonamesLocations: props.geonamesLocations,
     });
@@ -18,24 +20,13 @@
 
   Preferences.prototype.init = function() {
     this._hideButton.init();
+    this._themeInputs.init();
     this._geonamesInputs.init();
-    this._themeFieldsetElement.addEventListener('change', this._changeTheme.bind(this));
-    this._themeAttrs.on('change:value', this._updateTheme.bind(this));
     this._hideButton.on('click', this._events.emit.bind(this._events, 'hide'));
   };
 
   Preferences.prototype.on = function() {
     return Events.prototype.on.apply(this._events, arguments);
-  };
-
-  Preferences.prototype._changeTheme = function() {
-    var el = this._themeFieldsetElement.querySelector('input:checked');
-    this._themeAttrs.set('value', el.value);
-  };
-
-  Preferences.prototype._updateTheme = function(value) {
-    var el = this._themeFieldsetElement.querySelector('input[value=\'' + value + '\']');
-    el.checked = true;
   };
 
   if (typeof module !== 'undefined' && module.exports) {
---
+++ js/components/theme-inputs.js
@@ -0,0 +1,29 @@
+(function(app) {
+  'use strict';
+
+  var ThemeInputs = function(el, props) {
+    this.el = el;
+    this._themeAttrs = props.themeAttrs;
+  };
+
+  ThemeInputs.prototype.init = function() {
+    this.el.addEventListener('change', this._change.bind(this));
+    this._themeAttrs.on('change:value', this._update.bind(this));
+  };
+
+  ThemeInputs.prototype._change = function() {
+    var el = this.el.querySelector('input:checked');
+    this._themeAttrs.set('value', el.value);
+  };
+
+  ThemeInputs.prototype._update = function(value) {
+    var el = this.el.querySelector('input[value=\'' + value + '\']');
+    el.checked = true;
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = ThemeInputs;
+  } else {
+    app.ThemeInputs = ThemeInputs;
+  }
+})(this.app || (this.app = {}));
