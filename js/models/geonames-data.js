3a1caf79f987bfa48d53ae06e1a4156fd09a313a
nasitra <nasitra@users.noreply.github.com>
2022-11-07 18:29:40 +0900

    Make events of GeoNames data

--- js/models/geonames-data.js
+++ js/models/geonames-data.js
@@ -2,17 +2,25 @@
   'use strict';
 
   var dom = app.dom || require('../dom.js');
+  var Events = app.Events || require('../base/events.js');
 
   var GeoNamesData = function(url) {
     this._url = url;
     this._loadPromise = null;
     this._data = [];
+    this._events = new Events();
+  };
+
+  GeoNamesData.prototype.on = function() {
+    return Events.prototype.on.apply(this._events, arguments);
   };
 
   GeoNamesData.prototype.load = function() {
     if (!this._loadPromise) {
+      this._events.emit('loading');
       this._loadPromise = dom.loadJSON(this._url).then(function(data) {
         this._data = data;
+        this._events.emit('loaded');
       }.bind(this));
     }
     return this._loadPromise;
